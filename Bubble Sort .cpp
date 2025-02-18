23413b0d350058f948eece3f61f23abb0eadbafd
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2025-02-18 03:00:01 +0200

    Add files via upload

---
+++ Bubble Sort .cpp
@@ -0,0 +1,33 @@
+// Sorting Technique
+// Author :  ahmed hussein ahmed
+// Date :  18/02/2025
+
+#include <bits/stdc++.h>
+using namespace std ;
+// worst Case : O(n^2)
+// Best Case : O(n) if it is arranged at all
+
+void Bubble_Sort(int arr[] , int n ){
+    bool flag ;
+    for(int i = 0 ; i < n-1; i++){
+        flag = false ;
+        for(int j = 0 ; j < n-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+
+                flag = true;
+            }
+        }
+        if(!flag){
+            break;
+        }
+    }
+}
+int main(){
+    int arr[]={8,5,7,3,2};
+    Bubble_Sort(arr,5);
+
+    for(int i : arr){
+        cout<< i << ' ';
+    }
+}
